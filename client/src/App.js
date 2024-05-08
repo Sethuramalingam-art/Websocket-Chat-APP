636094c44b36778296f2c1e8dfbe084bb6e19b4e
sethu <callmesethu@gmail.com>
2024-05-09 00:33:33 +0530

    build failed fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
+import Home from "../src/pages/Home";
+import Chat from "../src/pages/Chat";
 
 function App() {
   return (
